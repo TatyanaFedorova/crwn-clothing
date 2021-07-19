9077bf60153868b225751ebb9268581f7990a7ad
Tatiana Fedorova <tfedorova1979@gmail.com>
2021-07-19 21:21:56 +1200

    New components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
         <Route path="/" component={HomePage} />
       </Switch>
     </div>
